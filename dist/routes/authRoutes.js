3f24da48362135e5647513285bf78ee694757c83
Samu Kangas <samu.kangas@metropolia.fi>
2024-11-19 20:07:03 +0200

    Päivitetty .gitignore

--- dist/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-// src/routes/authRoutes.ts
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const authController_1 = require("../controllers/authController");
-const router = (0, express_1.Router)();
-// POST /api/auth/login
-router.post('/login', authController_1.login);
-exports.default = router;
